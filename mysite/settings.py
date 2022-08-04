5d5cc60dec882ee549302b9f22c0112120ee569c
ParksPC1 <110574591+ParksPC1@users.noreply.github.com>
2022-08-03 17:32:36 -0700

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-fhb^v)=q=mbk+b3^%ml=f=d#j!^tv#z-&%tcuz6ccn=qb=8^19
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', "nvvhate.herokuapp.com/"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
